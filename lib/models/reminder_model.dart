929706b77a8aac1d3fad87c634a3e0e6ff4fc563
korekorokero <koresy.samuel20@gmail.com>
2025-05-19 22:58:20 +0700

    removed unnecessary variable

--- lib/models/reminder_model.dart
+++ lib/models/reminder_model.dart
@@ -2,26 +2,22 @@ class ReminderModel {
   final int id;
   final String title;
   final DateTime scheduledDate;
-  final bool isCompleted;
 
   ReminderModel({
     this.id = 0,
     required this.title,
     required this.scheduledDate,
-    this.isCompleted = false,
   });
 
   static ReminderModel fromJson(Map<String, dynamic> json) => ReminderModel(
     id: json['id'] as int,
     title: json['title'] as String,
     scheduledDate: DateTime.parse(json['scheduledDate'] as String),
-    isCompleted: json['isCompleted'] as bool? ?? false,
   );
 
   Map<String, dynamic> toJson() => {
     'id': id,
     'title': title,
     'scheduledDate': scheduledDate.toIso8601String(),
-    'isCompleted': isCompleted,
   };
 }
